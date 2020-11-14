b0c6c08902cb0747b8fdd8e94ce5aced7bc095a1
Pavel <pavelbesedinsky@gmail.com>
2020-11-15 00:37:02 +0300

    #lab3_besedinsky доработан клиент

--- lecture04/src/main/java/ru/atom/chat/client/ChatClient.java
+++ lecture04/src/main/java/ru/atom/chat/client/ChatClient.java
@@ -15,36 +15,92 @@ public class ChatClient {
     private static final String PROTOCOL = "http://";
     private static final String HOST = "localhost";
     private static final String PORT = ":8080";
+    private static final String PREFIX = "/api";
 
-    //POST host:port/chat/login?name=my_name
+    //POST host:port/api/chat/login?name=my_name
     public static Response login(String name) throws IOException {
         MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
         Request request = new Request.Builder()
                 .post(RequestBody.create(mediaType, ""))
-                .url(PROTOCOL + HOST + PORT + "/chat/login?name=" + name)
+                .url(PROTOCOL + HOST + PORT + PREFIX + "/chat/login?name=" + name)
                 .build();
 
         return client.newCall(request).execute();
     }
 
-    //GET host:port/chat/chat
+    //GET host:port/api/chat/chat
     public static Response viewChat() throws IOException {
         Request request = new Request.Builder()
                 .get()
-                .url(PROTOCOL + HOST + PORT + "/chat/chat")
+                .url(PROTOCOL + HOST + PORT + PREFIX + "/chat/chat")
                 .addHeader("host", HOST + PORT)
                 .build();
         return client.newCall(request).execute();
     }
 
-    //POST host:port/chat/say?name=my_name
+    //POST host:port/api/chat/say?name=my_name
     //Body: "msg='my_message'"
     public static Response say(String name, String msg) throws IOException {
-        throw new UnsupportedOperationException();
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        Request request = new Request.Builder()
+                .post(RequestBody.create(mediaType, ""))
+                .url(PROTOCOL + HOST + PORT + PREFIX + "/chat/say?name=" + name + "&msg=" + msg)
+                .build();
+
+        return client.newCall(request).execute();
     }
 
-    //GET host:port/chat/online
+    //GET host:port/api/chat/online
     public static Response viewOnline() throws IOException {
-        throw new UnsupportedOperationException();
+        Request request = new Request.Builder()
+                .get()
+                .url(PROTOCOL + HOST + PORT + PREFIX + "/chat/online")
+                .addHeader("host", HOST + PORT)
+                .build();
+        return client.newCall(request).execute();
+    }
+
+    //POST host:port/api/chat/logout
+    public static Response logout(String name) throws IOException {
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        Request request = new Request.Builder()
+                .post(RequestBody.create(mediaType, ""))
+                .url(PROTOCOL + HOST + PORT + PREFIX + "/chat/logout?name=" + name)
+                .build();
+
+        return client.newCall(request).execute();
+    }
+
+    //POST host:port/api/chat/ban
+    public static Response ban(String name) throws IOException {
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        Request request = new Request.Builder()
+                .post(RequestBody.create(mediaType, ""))
+                .url(PROTOCOL + HOST + PORT + PREFIX + "/chat/ban?name=" + name)
+                .build();
+
+        return client.newCall(request).execute();
+    }
+
+    //POST host:port/api/chat/unban
+    public static Response unban(String name) throws IOException {
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        Request request = new Request.Builder()
+                .post(RequestBody.create(mediaType, ""))
+                .url(PROTOCOL + HOST + PORT + PREFIX + "/chat/unban?name=" + name)
+                .build();
+
+        return client.newCall(request).execute();
+    }
+
+    //GET host:port/api/chat/clear
+    public static Response clear() throws IOException {
+        MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+        Request request = new Request.Builder()
+                .get()
+                .url(PROTOCOL + HOST + PORT + PREFIX + "/chat/clear")
+                .build();
+
+        return client.newCall(request).execute();
     }
 }
