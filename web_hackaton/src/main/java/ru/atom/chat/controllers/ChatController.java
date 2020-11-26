ff6ff7fa96e1a28a0bb503c8ef1bfbaac5226ec9
Vladislav Bykov <vlades7@mail.ru>
2020-11-26 18:21:28 +0300

    Добавлена CORS-police

--- web_hackaton/src/main/java/ru/atom/chat/controllers/ChatController.java
+++ web_hackaton/src/main/java/ru/atom/chat/controllers/ChatController.java
@@ -1,7 +1,5 @@
 package ru.atom.chat.controllers;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +9,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
@@ -20,46 +21,81 @@ import java.util.stream.Collectors;
 @Controller
 @RequestMapping("chat")
 public class ChatController {
-    private static final Logger log = LoggerFactory.getLogger(ChatController.class);
-
     private Queue<String> messages = new ConcurrentLinkedQueue<>();
+    private Map<String, String> registeredUsers = new ConcurrentHashMap<>();
     private Map<String, String> usersOnline = new ConcurrentHashMap<>();
+    private String adminPassword = getSha256("admin");
+
+    ChatController() {
+        registeredUsers.put("admin", adminPassword);
+    }
 
     /**
-     * curl -X POST -i localhost:8080/chat/login -d "name=I_AM_STUPID"
+     * curl -X POST -i localhost:8080/chat/signup -d "name=NAME&password=PASSWORD"
+     */
+    @RequestMapping(
+            path = "signup",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity<String> signup(@RequestParam("name") String name,
+                                         @RequestParam("password") String password) {
+        if (name.length() < 1) {
+            return ResponseEntity.badRequest().body("Too short name.");
+        }
+
+        if (name.length() > 20) {
+            return ResponseEntity.badRequest().body("Too long name.");
+        }
+
+        if (password.length() < 8) {
+            return ResponseEntity.badRequest().body("Too short password.");
+        }
+
+        if (password.length() > 16) {
+            return ResponseEntity.badRequest().body("Too long password.");
+        }
+
+        if (registeredUsers.containsKey(name)) {
+            return ResponseEntity.badRequest().body("This user already exists");
+        }
+
+        if (!registeredUsers.containsKey("admin")) {
+            registeredUsers.put("admin", getSha256(adminPassword));
+        }
+
+        registeredUsers.put(name, getSha256(password));
+        messages.add("[" + name + "] registered! Welcome to our chat!");
+
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * curl -X POST -i localhost:8080/chat/login -d "name=NAME&password=PASSWORD"
      */
     @RequestMapping(
             path = "login",
             method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity<String> login(@RequestParam("name") String name) {
-        if (name.length() < 1) {
-            return ResponseEntity.badRequest().body("Too short name, sorry :(");
+    public ResponseEntity<String> login(@RequestParam("name") String name,
+                                        @RequestParam("password") String password) {
+        if (!registeredUsers.containsKey(name)) {
+            return ResponseEntity.badRequest().body("This user doesn't exists");
         }
-        if (name.length() > 20) {
-            return ResponseEntity.badRequest().body("Too long name, sorry :(");
-        }
-        if (usersOnline.containsKey(name)) {
-            return ResponseEntity.badRequest().body("Already logged in:(");
-        }
-        usersOnline.put(name, name);
-        messages.add("[" + name + "] logged in");
-        return ResponseEntity.ok().build();
-    }
 
-    /**
-     * curl -i localhost:8080/chat/chat
-     */
-    @RequestMapping(
-            path = "chat",
-            method = RequestMethod.GET,
-            produces = MediaType.TEXT_PLAIN_VALUE)
-    public ResponseEntity<String> chat() {
-        return new ResponseEntity<>(messages.stream()
-                .map(Object::toString)
-                .collect(Collectors.joining("\n")),
-                HttpStatus.OK);
+        if (usersOnline.containsKey(name)) {
+            return ResponseEntity.badRequest().body("Already logged in.");
+        }
+
+        if ((name.equals("admin")) && (!getSha256(password).equals(adminPassword))) {
+            return ResponseEntity.badRequest().body("Incorrect Admin password.");
+        }
+
+        usersOnline.put(name, getSha256(password));
+        messages.add("[" + name + "] logged in");
+
+        return ResponseEntity.ok().build();
     }
 
     /**
@@ -70,31 +106,151 @@ public class ChatController {
             method = RequestMethod.GET,
             produces = MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity online() {
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);//TODO
+        String responseBody = String.join("\n", usersOnline.keySet().stream()
+                .sorted().collect(Collectors.toList()));
+
+        return ResponseEntity.ok(responseBody);
     }
 
     /**
-     * curl -X POST -i localhost:8080/chat/logout -d "name=I_AM_STUPID"
+     * curl -i localhost:8080/chat/allusers
+     */
+    @RequestMapping(
+            path = "allusers",
+            method = RequestMethod.GET,
+            produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity allUsers() {
+        String responseBody = String.join("\n", registeredUsers.keySet().stream()
+                .sorted().collect(Collectors.toList()));
+
+        return ResponseEntity.ok(responseBody);
+    }
+
+    /**
+     * curl -X POST -i localhost:8080/chat/logout -d "name=NAME&password=PASSWORD"
      */
     @RequestMapping(
             path = "logout",
             method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity logout(@RequestParam("name") String name) {
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);//TODO
+    public ResponseEntity<String> logout(@RequestParam("name") String name,
+                                         @RequestParam("password") String password) {
+        if (!usersOnline.containsKey(name)) {
+            return ResponseEntity.badRequest().body("Already logged out.");
+        }
+
+        if (getSha256(password).equals(usersOnline.get(name))) {
+            usersOnline.remove(name);
+            messages.add("[" + name + "] logged out.");
+        } else {
+            return ResponseEntity.badRequest().body("Wrong password.");
+        }
+
+        return ResponseEntity.ok().build();
     }
 
-
     /**
-     * curl -X POST -i localhost:8080/chat/say -d "name=I_AM_STUPID&msg=Hello everyone in this chat"
+     * curl -X POST -i localhost:8080/chat/say -d "name=NAME&password=PASSWORD&msg=MESSAGE"
      */
     @RequestMapping(
             path = "say",
             method = RequestMethod.POST,
             consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
     @ResponseStatus(HttpStatus.OK)
-    public ResponseEntity say(@RequestParam("name") String name, @RequestParam("msg") String msg) {
-        return new ResponseEntity<>(HttpStatus.BAD_REQUEST);//TODO
+    public ResponseEntity<String> say(@RequestParam("name") String name,
+                                      @RequestParam("password") String password,
+                                      @RequestParam("msg") String msg) {
+        if (!usersOnline.containsKey(name)) {
+            return ResponseEntity.badRequest().body("User logged out.");
+        }
+
+        if (getSha256(password).equals(usersOnline.get(name))) {
+            messages.add("[" + name + "]: " + msg);
+        } else {
+            return ResponseEntity.badRequest().body("Wrong password.");
+        }
+
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * curl -i localhost:8080/chat/chat
+     */
+    @RequestMapping(
+            path = "chat",
+            method = RequestMethod.GET,
+            produces = MediaType.TEXT_PLAIN_VALUE)
+    public ResponseEntity chat() {
+        String responseBody = String.join("\n", messages);
+
+        return ResponseEntity.ok(responseBody);
+    }
+
+    /**
+     * curl -X POST -i localhost:8080/chat/admin/kick -d "adminname=ADMINNAME&password=PASSWORD&kickname=KICKNAME"
+     */
+    @RequestMapping(
+            path = "admin/kick",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity adminKick(@RequestParam("adminname") String adminName,
+                                    @RequestParam("password") String password,
+                                    @RequestParam("kickname") String kickName) {
+        if ((!adminName.equals("admin")) || (!getSha256(password).equals(adminPassword))) {
+            return ResponseEntity.badRequest().body("Incorrect Admin password.");
+        }
+
+        if (!usersOnline.containsKey(kickName)) {
+            return ResponseEntity.badRequest().body("User logged out.");
+        }
+
+        usersOnline.remove(kickName);
+        messages.add("[" + kickName + "] was kicked by Admin.");
+
+        return ResponseEntity.ok().build();
+    }
+
+    /**
+     * curl -X POST -i localhost:8080/chat/admin/ban -d "adminname=ADMINNAME&password=PASSWORD&banname=BANNAME"
+     */
+    @RequestMapping(
+            path = "admin/ban",
+            method = RequestMethod.POST,
+            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity adminBan(@RequestParam("adminname") String adminName,
+                                   @RequestParam("password") String password,
+                                   @RequestParam("banname") String banName) {
+        if ((!adminName.equals("admin")) || (!getSha256(password).equals(adminPassword))) {
+            return ResponseEntity.badRequest().body("Incorrect Admin password.");
+        }
+
+        usersOnline.remove(banName);
+
+        if (!registeredUsers.containsKey(banName)) {
+            return ResponseEntity.badRequest().body("[" + banName + "] doesn't exists");
+        }
+
+        registeredUsers.remove(banName);
+        messages.add("[" + banName + "] was banned by Admin. Bye-bye!");
+
+        return ResponseEntity.ok().build();
+    }
+
+    private String getSha256(String str) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] messageDigest = md.digest(str.getBytes());
+            BigInteger no = new BigInteger(1, messageDigest);
+
+            String hashText = no.toString(16);
+            while (hashText.length() < 32) {
+                hashText = "0" + hashText;
+            }
+
+            return hashText;
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
